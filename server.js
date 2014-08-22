823e5c04f215eb94ba8808a3d32bba27d67281c2
Gautam Mittal <gautam@mittal.net>
2014-08-22 15:55:15 -0700

    email sending set up

--- server.js
+++ server.js
@@ -7,46 +7,106 @@ var twilio = require('twilio');
 var app = express();
 
 // SMTP
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'pinpoint.smtp@gmail.com', // change this
-        pass: 'PinpointServer' // change this as well
-    }
-});
 
 
 
-// app.use(express.bodyParser());
-app.use(bodyParser());       // to support JSON-encoded bodies
-// app.use( express.urlencoded() ); // to support URL-encoded bodies
 
-app.all('/', function(req, res){
-	// console.log(req.body);
+app.use(bodyParser());      
 
 
+// Email Blaster
+app.post('/email_blaster', function(req, res){
 
-});
+
+	var messageBody = req.body.text;
+	var sender = req.body.senderName + '<'+ req.body.senderEmail +'>';
+	var subjectLine = req.body.subject;
+
+	var recipients = eval(req.body.addressList);
 
 
-
-function sendEmail(email, msgBody) {
-	var mailOptions = {
-	    from: 'Pinpoint <no-reply@gmail.com>', // sender address
-	    to: email, // list of receivers
-	    subject: 'Pinpoint: Somebody Has Shared Their Location With You', // Subject line
-	    text: msgBody
-	};
-
-	// send mail with defined transport object
-	transporter.sendMail(mailOptions, function(error, info){
-	    if(error){
-	        console.log(error);
-	    }else{
-	        console.log('Message sent: ' + info.response);
+	var transporter = nodemailer.createTransport({
+	    service: req.body.senderService,
+	    auth: {
+	        user: req.body.senderEmail, // change this
+	        pass: req.body.senderPassword // change this as well
 	    }
 	});
-}
+
+
+	for (var user = 0; user < recipients.length; user++) {
+
+		var mailOptions = {
+		    from: sender, // sender address
+		    to: recipients[user], // list of receivers
+		    subject: subjectLine, // Subject line
+		    text: messageBody
+		};
+
+		// send mail with defined transport object
+		transporter.sendMail(mailOptions, function(error, info){
+		    if(error){
+		        console.log(error);
+		        res.send(error);
+		    }else{
+		        console.log('Message sent: ' + info.response);
+		        res.send('Message sent: ' + info.response);
+		    }
+		});
+	}
+
+
+
+
+
+});
+
+
+// Simple request to send a single email
+app.post('/send_email', function(req, res){
+
+
+	var messageBody = req.body.text;
+	var sender = req.body.senderName + '<'+ req.body.senderEmail +'>';
+	var subjectLine = req.body.subject;
+
+	var recipient = req.body.address;
+
+
+	var transporter = nodemailer.createTransport({
+	    service: req.body.senderService,
+	    auth: {
+	        user: req.body.senderEmail, // change this
+	        pass: req.body.senderPassword // change this as well
+	    }
+	});
+
+
+
+		var mailOptions = {
+		    from: sender, // sender address
+		    to: recipient, // list of receivers
+		    subject: subjectLine, // Subject line
+		    text: messageBody
+		};
+
+		// send mail with defined transport object
+		transporter.sendMail(mailOptions, function(error, info){
+		    if(error){
+		        console.log(error);
+		        res.send(error);
+		    }else{
+		        console.log('Message sent: ' + info.response);
+		        res.send('Message sent: ' + info.response);
+		    }
+		});
+
+
+});
+
+
+
+
 
 
 
